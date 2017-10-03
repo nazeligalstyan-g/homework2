27320999f728beb7c40a6acac397badf42d2b834
Nazeli Galstyan <nazeli.galstyan.g@gmail.com>
2017-10-03 17:57:11 +0400

    homework2

---
+++ js/homework2.js
@@ -0,0 +1,37 @@
+/*Decimal number is converted to the binary number*/
+const number = 30;
+const divider = 2;
+const binary_digit_1 = 30/2;
+const binary_digit_2 = binary_digit_1/2;
+const binary_digit_3 = binary_digit_2/2;
+const binary_digit_4 = binary_digit_3/2;
+const binary_digit_5 = binary_digit_4/2;
+
+
+console.log(binary_digit_1, binary_digit_2, binary_digit_3, binary_digit_4, binary_digit_5);
+console.log("This will be converted binary number " + 11110);
+
+/*Binary number is converted to the decimal number */
+
+const dec_digit_1 = (Math.pow(2,0))*0;
+const dec_digit_2 = (Math.pow(2,1))*1;
+const dec_digit_3 = (Math.pow(2,2))*1;
+const dec_digit_4 = (Math.pow(2,3))*1;
+const dec_digit_5 = (Math.pow(2,4))*1;
+
+console.log(dec_digit_1, dec_digit_2, dec_digit_3, dec_digit_4, dec_digit_5);
+const total_dec_number = dec_digit_1 + dec_digit_2 + dec_digit_3 + dec_digit_4 + dec_digit_5;
+
+console.log("This will be converted decimal number");
+console.log(total_dec_number);
+
+
+
+
+
+
+
+
+
+
+
